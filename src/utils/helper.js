d21ee768ce4dc361a8918b8b555f6d1682efb142
Meet Panchal <meet.panchal@bacancy.com>
2021-05-04 11:58:46 +0530

    more helper functions added

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,33 @@
+import socketIOClient from 'socket.io-client';
+
+export const socket = socketIOClient(process.env.REACT_APP_SOCKET_URL, {
+  transports: ['websocket'],
+  reconnectionAttempts: 20,
+  reconnectionDelay: 5000,
+});
+
+export const debounce = (callback, wait) => {
+  let timeout = null;
+  return (...args) => {
+    const next = () => callback(...args);
+    clearTimeout(timeout);
+    timeout = setTimeout(next, wait);
+  };
+};
+
+export const getSentenceFromCamelCase = (msg) => {
+  const patt1 = /[A-Za-z]/g;
+  const msgArr = msg.match(patt1);
+  let errorMsg = '';
+  for (let i = 0; i < msgArr.length; i += 1) {
+    errorMsg +=
+      msgArr[i] === msgArr[i].toUpperCase()
+        ? ` ${msgArr[i].toLowerCase()}`
+        : msgArr[i];
+  }
+  return errorMsg.trim();
+};
+
 export const getItemFromStorage = (key) => {
   const item = window.localStorage.getItem(key);
   return item ? JSON.parse(item) : null;
