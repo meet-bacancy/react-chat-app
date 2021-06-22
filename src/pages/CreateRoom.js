38731460b6cb522822823759169bd5c6222536ff
Meet Panchal <meet.panchal@bacancy.com>
2021-06-22 11:45:00 +0530

    unused scss removed

--- src/pages/CreateRoom.js
+++ src/pages/CreateRoom.js
@@ -25,7 +25,7 @@ import { DEFAULT_ROOM_TYPE, ROOM_TYPE } from '../utils/constants';
 import { toastError, toastSuccess } from '../components/notifications';
 
 const CreateRoom = () => {
-  const { name } = getItemFromStorage('user');
+  const user = getItemFromStorage('user');
   const [roomId, setRoomId] = useState('');
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [roomType, setRoomType] = useState(DEFAULT_ROOM_TYPE);
@@ -60,74 +60,76 @@ const CreateRoom = () => {
   };
   const toggle = () => setDropdownOpen(!dropdownOpen);
 
-  return (
-    <Container>
-      <Row className="justify-content-center align-items-center h-100vh">
-        <Col sm="12" md={6}>
-          <Card>
-            <CardBody>
-              <CardTitle tag="h3" className="text-center mb-5">
-                React Chat App
-              </CardTitle>
-              <Form onSubmit={onLoginClick}>
-                <h5>{`Welcome, ${name}`}</h5>
-                <>
-                  {roomType === ROOM_TYPE.joinRoom ? (
-                    <FormGroup>
-                      <Label for="roomId">Room-id</Label>
-                      <Input
-                        type="text"
-                        name="roomId"
-                        id="roomId"
-                        placeholder="Enter room id"
-                        value={roomId}
-                        onChange={(e) => setRoomId(e.target.value)}
-                        required
+  if (user && user.name)
+    return (
+      <Container>
+        <Row className="justify-content-center align-items-center h-100vh">
+          <Col sm="12" md={6}>
+            <Card>
+              <CardBody>
+                <CardTitle tag="h3" className="text-center mb-5">
+                  React Chat App
+                </CardTitle>
+                <Form onSubmit={onLoginClick}>
+                  <h5>{`Welcome, ${user.name}`}</h5>
+                  <>
+                    {roomType === ROOM_TYPE.joinRoom ? (
+                      <FormGroup>
+                        <Label for="roomId">Room-id</Label>
+                        <Input
+                          type="text"
+                          name="roomId"
+                          id="roomId"
+                          placeholder="Enter room id"
+                          value={roomId}
+                          onChange={(e) => setRoomId(e.target.value)}
+                          required
+                        />
+                      </FormGroup>
+                    ) : null}
+                  </>
+                  <InputGroup>
+                    <InputGroupButtonDropdown
+                      addonType="prepend"
+                      isOpen={dropdownOpen}
+                      toggle={toggle}
+                    >
+                      <Button color="secondary" outline disabled={isLoading}>
+                        {isLoading ? (
+                          <Spinner className="mr-2" size="sm" />
+                        ) : null}
+                        {roomType === ROOM_TYPE.joinRoom
+                          ? 'Join room'
+                          : 'Create room'}
+                      </Button>
+                      <DropdownToggle
+                        outline
+                        disabled={isLoading}
+                        split
+                        color="secondary"
                       />
-                    </FormGroup>
-                  ) : null}
-                </>
-                <InputGroup>
-                  <InputGroupButtonDropdown
-                    addonType="prepend"
-                    isOpen={dropdownOpen}
-                    toggle={toggle}
-                  >
-                    <Button color="secondary" outline disabled={isLoading}>
-                      {isLoading ? (
-                        <Spinner className="mr-2" size="sm" />
-                      ) : null}
-                      {roomType === ROOM_TYPE.joinRoom
-                        ? 'Join room'
-                        : 'Create room'}
-                    </Button>
-                    <DropdownToggle
-                      outline
-                      disabled={isLoading}
-                      split
-                      color="secondary"
-                    />
-                    <DropdownMenu>
-                      <DropdownItem
-                        onClick={() => setRoomType(ROOM_TYPE.createRoom)}
-                      >
-                        Create Room
-                      </DropdownItem>
-                      <DropdownItem
-                        onClick={() => setRoomType(ROOM_TYPE.joinRoom)}
-                      >
-                        Join Room
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </InputGroupButtonDropdown>
-                </InputGroup>
-              </Form>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
+                      <DropdownMenu>
+                        <DropdownItem
+                          onClick={() => setRoomType(ROOM_TYPE.createRoom)}
+                        >
+                          Create Room
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => setRoomType(ROOM_TYPE.joinRoom)}
+                        >
+                          Join Room
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </InputGroupButtonDropdown>
+                  </InputGroup>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  return null;
 };
 
 export default CreateRoom;
